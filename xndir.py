25419fa4c376971216accecdd4c2124561096c14
ztnspheroid <ztnspheroid@gmail.com>
2023-05-03 17:35:09 +0400

    lesson_func

---
+++ xndir.py
@@ -0,0 +1,33 @@
+# n=int(input())
+
+# def rrr():
+#     l=[i for i in str(n)]
+#     if l[-1]=='0':
+#         del l[-1]
+#         l.reverse()
+#     else:
+#         l.reverse()
+#     return l
+
+# if n!=0:
+#     print(*rrr(),sep='')
+# else:
+#     print('Программа завершена!')
+#-----------------------------------------------
+# s=input('Введите текст: ')
+# n=int(input('Какую цифру ищем?'))
+# b=input('Какую букву ищем?')
+# l=[i for i in s]
+# def func():
+#     for i in l:
+#         if i==str(n):
+#             x=l.count(str(n))
+#             return x
+# def func1():
+#     for i in l:   
+#         if i==b:
+#             y=l.count(b)
+#             return y
+# print(f'Количество цифр {n} : {func()}')
+# print(f'Количество bukv {b} : {func1()}')
+#---------------------------------------------
